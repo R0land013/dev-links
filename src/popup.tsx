8c81a1cdbd98dc398954c93d5b5dc6a606769118
Rolando Rio <rolandorio06@gmail.com>
2023-08-17 12:08:32 -0400

    Fix: Firefox can not open the link browse tab programatically as Chrome, Opera, Brave and Edge do
    	For this reason the popup menu will only appear for firefox user for now, still
    	a workaround is found.
    
    	Added firefox addon id to manifest, so the users of firefox browser can synchronize the
    	saved data of Dev Links. The Storage of plasmo always stores data in sync space.

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,13 +1,53 @@
 import { useEffect } from "react";
+import Button from "~components/Button";
+import OpenIcon from "data-base64:~assets/icons/general/open.svg";
+import LogoIcon from "data-base64:~assets/icons/general/logo.svg";
+import "./style.css";
 
 
 function IndexPopup() {
 
     useEffect(() => {
-        window.open('./tabs/browse-links.html');
-    });
+        if(!navigator.userAgent.toLowerCase().includes('fox')) {
+            window.open('./tabs/browse-links.html');
+        }
+    }, []);
 
-    return (<></>);
+    return (<div className="w-56 h-56 flex flex-col items-center justify-center bg-app-background">
+
+        <div className="flex flex-col w-full items-center">
+
+            <div className="flex flex-row items-center justify-center">
+
+                <img
+                    src={LogoIcon}
+                    className="w-8" />
+
+                <h1 className="text-header-color font-bold text-3xl">Dev Links</h1>
+
+            </div>
+
+            <span className="text-header-color text-[0.75rem] mt-1">
+                All links a developer would need
+            </span>
+
+        </div>
+
+        <Button
+            onClick={() => {
+                window.open('./tabs/browse-links.html');
+            }}
+            className="mt-10">
+
+            <span className="font-bold text-base">
+                Browse Links!
+            </span>
+
+            <img
+                src={OpenIcon}
+                className="w-5 ml-1" />
+        </Button>
+    </div>);
 }
 
 export default IndexPopup;
